320052d8d44c9aa5f24e49db55cedb24babc609a
Layth Barzangi <thebarzangi@gmail.com>
2021-11-17 21:24:05 +0400

    Implemented functionality to update `added_todos` key in redis db when new todo is added

--- todo-app/todo-backend/routes/todos.js
+++ todo-app/todo-backend/routes/todos.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { Todo } = require('../mongo');
 const router = express.Router();
+const { getAsync, setAsync } = require('../redis');
 
 /* GET todos listing. */
 router.get('/', async (_, res) => {
@@ -14,6 +15,9 @@ router.post('/', async (req, res) => {
     text: req.body.text,
     done: false,
   });
+  let addedTodos = await getAsync('added_todos');
+  addedTodos++;
+  await setAsync('added_todos', addedTodos);
   res.send(todo);
 });
 
